104f6c2530513cfe57b01b8f18b0c78d21e5abd7
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-08-09 00:08:24 -0400

    Updated messages in static resources

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/AbstractProgramValidatorVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/validate/AbstractProgramValidatorVisitor.java
@@ -648,7 +648,7 @@ public abstract class AbstractProgramValidatorVisitor extends AbstractCollectorV
     @Override
     public Void visitEvalExpr(EvalExpr<Void> evalExpr) {
         if ( evalExpr.hasSyntaxError() ) {
-            addError(Key.EXPRBLOCK_PARSE_ERROR.getKey(), evalExpr);
+            addError(Key.COMPILERWORKFLOW_ERROR_PROGRAM_NOT_FOUND.getKey(), evalExpr);
         } else {
             int i = 0;
             ArrayList<VarDeclaration<Void>> vars = this.getVisitedVars();
